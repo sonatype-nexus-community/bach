d1cbcca06057c03d61b89a95a963d81b01a04a84
Allister Beharry <allister.beharry@gmail.com>
2019-10-03 19:56:05 -0400

    Read transitive dependencies from composer.lock.

--- app/Commands/Composer.php
+++ app/Commands/Composer.php
@@ -36,7 +36,7 @@ class Composer extends Command
         parent::__construct();
         $this->styles = array
         (
-            'red' => new OutputFormatterStyle('red', null, ['bold']) //white text on red background
+            // 'red' => new OutputFormatterStyle('red', null, ['bold']) //white text on red background
         );
     }
 
@@ -65,7 +65,9 @@ class Composer extends Command
         {
             $this->output->getFormatter()->setStyle($key, $value);
         }
+
         $this->show_logo();
+        
         if (!File::exists($this->argument('file')))
         {
             $this->error("The file " . $this->argument('file') . " does not exist");
@@ -78,14 +80,24 @@ class Composer extends Command
             $this->error("Could not read composer file " . $this->argument('file') ."." );
             return;
         }
-                    
         $this->get_packages($reader);
-        $this->info("Parsed " . \count($this->packages) . " packages from " . $this->file . ":");
+        $this->comment("Parsed " . \count($this->packages) . " packages from " . $this->file . ":");
         if (count($this->packages) == 0)
         {
-            $this->error("No packages found to audit.");
+            $this->warn("No packages found to audit.");
             return;
         }
+
+        $this->lock_file = dirname($this->file). DIRECTORY_SEPARATOR . 'composer.lock';
+        if (File::exists($this->lock_file))
+        {
+            $this->comment("Composer lock file found at ".$this->lock_file). '.';
+            $this->get_lock_file_packages($this->lock_file);
+        }
+        else {
+            $this->warn("Did not find composer lock file found at ".$this->lock_file). '.' . ' Transitive package dependencies will not be audited.';
+        }            
+        $this->comment(count($this->packages) . " total packages to audit:");
         $this->get_packages_versions();
         $this->get_coordinates();
         $this->get_vulns();
@@ -96,8 +108,8 @@ class Composer extends Command
         }
         else
         {
-            $this->info("");
-            $this->info("Audit results:");
+            $this->comment("");
+            $this->comment("Audit results:");
             $this->info("==============");
             foreach($this->vulnerabilities as $v)
             {
@@ -109,14 +121,16 @@ class Composer extends Command
                 $d = array_key_exists("description", $v) ? "DESC: " . $v['description'] : "";
                 $is_vulnerable = array_key_exists("vulnerabilities", $v) ? (count($v['vulnerabilities']) > 0 ? true: false) : false;
                 $is_vulnerable_text = "VULN: " . ($is_vulnerable ? "Yes" : "No");
-                $this->info($p . " " . $d . " " . $is_vulnerable_text);
-                if ($is_vulnerable)
-                {
+                if(!$is_vulnerable) {
+                    $this->info($p . " " . $d . " " . $is_vulnerable_text);
+                }
+                else {
+                    $this->error($p . " " . $d . " " . $is_vulnerable_text);
                     foreach($v["vulnerabilities"] as $vuln)
                     {
                         foreach($vuln as $key => $value)
                         {
-                            $this->info("  " . $key . ":".$value);
+                            $this->comment("  " . $key . ": ".$value);
                         }
                     }
                 }                
@@ -126,7 +140,7 @@ class Composer extends Command
 
     protected function show_logo()
     {
-        $l = new ZendLogo('auditphp', []);
+        $l = new ZendLogo('Bach', []);
         echo $l;
     }
 
@@ -135,6 +149,59 @@ class Composer extends Command
         $section = new RequireSection($reader);
         foreach($section as $package) {
             $this->packages[$package->name] = $package->constraint;
+        }
+    }
+
+    protected function get_lock_file_packages($lock_file)
+    {
+        $orig_count = count($this->packages);
+        $data = \json_decode(\file_get_contents($lock_file), true);
+        $lfp = $data["packages"];
+        foreach ($lfp as $p)  {
+            $n = $p["name"];
+            $v = $p["version"];
+            if (!\array_key_exists($n, $this->packages))
+            { 
+                $this->packages[$n] = $v;
+
+            }
+            else if (array_key_exists($n, $this->packages) && $this->packages[$n] != $v)
+            { 
+                $this->packages[$n] = $v;
+            }
+
+            if (\array_key_exists("require", $p))
+            {
+                foreach ($p["require"] as $rn => $rv) {
+                    if (!\array_key_exists($rn, $this->packages))
+                    { 
+                        $this->packages[$rn] = $rv;
+                    }
+                    else if (array_key_exists($rn, $this->packages) && $this->packages[$rn] != $rv)
+                    { 
+                        $this->packages[$rn] = $rv;
+                    }
+                }
+            }
+
+            if (\array_key_exists("require-dev", $p))
+            {
+                foreach ($p["require-dev"] as $rn => $rv) {
+                    if (!\array_key_exists($rn, $this->packages))
+                    { 
+                        $this->packages[$rn] = $rv;
+                    }
+                    else if (array_key_exists($rn, $this->packages) && $this->packages[$rn] != $rv)
+                    { 
+                        $this->packages[$rn] = $rv;
+                    }
+                }
+            }
+        }
+        $count = count($this->packages);
+        if ($count > $orig_count)
+        {
+            $this->comment("Added ".($count - $orig_count). " packages from Composer lock file at ".$lock_file.".");
         }
     }
 
@@ -189,7 +256,7 @@ class Composer extends Command
         }
         else
         {
-            $this->warn("Could not determine version operator.");
+            $this->warn("Could not determine version operator for constraint ".$constraint.".");
             return $constraint;
         }
     }
@@ -198,9 +265,25 @@ class Composer extends Command
     {
         foreach($this->packages as $package=>$constraint) 
         {
-            $v = $this->get_version($constraint);
-            $this->info($package . ' ' .$constraint . ' (' .$v. ')');
-            $this->packages_versions[$package] = $v;
+            $c = $constraint;
+            if (strpos($constraint, "||") !== false) {
+                $c = trim(explode("||", $constraint)[0]);
+            }
+            else if (strpos($constraint, "|") !== false) {
+                $c = trim(explode("|", $constraint)[0]);
+            }
+            $c = trim($c, "v");
+            if ($c == '*')
+            {
+                $c = '0.1';
+            }
+            try {
+                $v = $this->get_version($c);
+                $this->info($package . ' ' .$constraint . ' (' .$v. ')');
+                $this->packages_versions[$package] = $v;
+            } catch (\Throwable $th) {
+                $this->error("Error occurred determinig version for package ".$package . "(".$constraint.")". ": ".$th->getMessage().".");
+            }
         }        
     }
 
@@ -211,7 +294,6 @@ class Composer extends Command
         {
             array_push($this->coordinates["coordinates"], "pkg:composer/" . $package . "@".$version);
         }
-        
     }
 
     protected function get_vulns()
@@ -247,12 +329,4 @@ class Composer extends Command
             return;
         }
     }
-
-    protected function console_write($string, $style = null, $verbosity = null)
-    {
-        $styled = $style ? "<$style>$string</$style>" : $string;
-
-        $this->output->write($styled, false, $this->parseVerbosity($verbosity));
-    }
-
 }
