6d45c89781d1f616c979bf9095a6c09c5e0b3762
Dan Rollo <drollo@sonatype.com>
2021-02-25 13:37:45 -0500

    Php 8.0.0 (#21)
    * cake and eat it too - build with php 7.4 and 8.0

--- config/app.php
+++ config/app.php
@@ -53,8 +53,7 @@ return [
     */
 
     'providers' => [
-        App\Providers\AppServiceProvider::class,
-        Intonate\TinkerZero\TinkerZeroServiceProvider::class
+        App\Providers\AppServiceProvider::class
     ],
 
 ];
