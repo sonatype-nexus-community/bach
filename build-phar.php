e6201176ee5d8ef1b460deb16942e5112ac4bdec
Paul Horton <phorton@sonatype.com>
2021-08-20 18:13:43 +0100

    Revert "- Added a bunch of exclusions to .gitignore"
    This reverts commit ea64010d657534369450b970376102d0982666e1.

--- build-phar.php
+++ build-phar.php
@@ -1,7 +1,8 @@
 <?php
 $srcRoot = __DIR__;
-$buildRoot = dirname(__FILE__) . DIRECTORY_SEPARATOR . "build" . DIRECTORY_SEPARATOR;
-
-$phar = new Phar($buildRoot . "bach.phar", FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, "bach.phar");
-$phar->buildFromDirectory(dirname(__FILE__));
+$buildRoot = "./build";
+ 
+$phar = new Phar("bach.phar", 
+	FilesystemIterator::CURRENT_AS_FILEINFO |     	FilesystemIterator::KEY_AS_FILENAME, "myapp.phar");
+	$phar->buildFromDirectory(dirname(__FILE__));
 $phar->setStub($phar->createDefaultStub("bach"));
